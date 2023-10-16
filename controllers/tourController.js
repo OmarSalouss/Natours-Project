3567fb8df7c70e10b232e31e6aac976cdbafe3d2
Omar <omar.salous2@gmail.com>
2023-10-16 21:13:58 +0300

    Filter GET All Tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,29 @@ const Tour = require('../models/tourModel');
 //! so here we can't have any block in code
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        //! BUILD QUERY
+        const queryObj = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => { delete queryObj[el] });
+        console.log(req.query, queryObj);
+
+        const query = Tour.find(queryObj);
+
+        // const tours = await Tour.find({
+        //     duration: 5,
+        //     difficulty: "easy"
+        // });
+
+        // const tours = await Tour.find()
+        // .where('duration').
+        // equals(5)
+        // .where('difficulty')
+        // .equals('easy');
+
+        //! EXECUTE QUERY
+        const tours = await query;
+
+        //! SEND RESPONSE
         res.status(200).json({
             status: "Success",
             results: tours.length,
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
