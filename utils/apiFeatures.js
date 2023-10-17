81f8bfc520a38d251939f638a0547807779b613f
Omar <omar.salous2@gmail.com>
2023-10-17 10:02:44 +0300

    Refactoring API Features

---
+++ utils/apiFeatures.js
@@ -0,0 +1,61 @@
+
+class APIFeatures {
+    constructor(query, queryString) {
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    filter() {
+        const queryObj = { ...this.queryString };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => { delete queryObj[el] });
+        // console.log(req.query, queryObj);
+
+        // 1B) Advanced Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log("***************");
+        console.log(JSON.parse(queryStr));
+        console.log(queryStr);
+        console.log("***************");
+
+        this.query = this.query.find(JSON.parse(queryStr));
+
+        return this;
+        // let query = Tour.find(JSON.parse(queryStr));
+    }
+
+    sort() {
+        if (this.query.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ');
+            this.query = this.query.sort(sortBy);
+        } else {
+            this.query = this.query.sort('-createdAt'); // Default Sort
+        }
+
+        return this;
+    }
+
+    limitFields() {
+        if (this.query.fields) {
+            const fields = this.query.fields.split(',').join(' ');
+            this.query = this.query.select(fields);
+        } else {
+            this.query = this.query.select('-__v'); //select everything exclude (__v) because i put '-'
+        }
+
+        return this;
+    }
+
+    paginate() {
+        const page = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        this.query = this.query.skip(skip).limit(limit);
+
+        return this;
+    }
+}
+
+module.exports = APIFeatures;
