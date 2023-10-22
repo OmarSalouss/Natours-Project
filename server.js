b4d53cadde73988cb0afd6dbeebd57da0a567311
Omar <omar.salous2@gmail.com>
2023-10-22 11:40:15 +0300

    Handle Uncaught Exception

--- server.js
+++ server.js
@@ -23,8 +23,20 @@ const server = app.listen(port, () => {
 
 
 process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 🔥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
-})
+  // EX: uncorrecet DATABASE PASSWORD
+});
+
+process.on('uncaughtException', err => {
+  console.log('UNHANDLED EXCEPTION! 🔥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+console.log(x);
