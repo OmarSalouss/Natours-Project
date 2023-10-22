c1663c5819359e198afd7b4f797ada710db9457d
Omar <omar.salous2@gmail.com>
2023-10-22 11:55:36 +0300

    Reorder of Handle Uncaught Exception and Unhandle Rejection

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const mongose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 🔥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+  // EX: uncorrecet DATABASE PASSWORD
+});
+
+process.on('uncaughtException', err => {
+  console.log('UNHANDLED EXCEPTION! 🔥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+  // EX: console.log(x); // x not define 
+});
+
+
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -21,22 +37,3 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
 
-
-process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION! 🔥 Shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-  // EX: uncorrecet DATABASE PASSWORD
-});
-
-process.on('uncaughtException', err => {
-  console.log('UNHANDLED EXCEPTION! 🔥 Shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-console.log(x);
