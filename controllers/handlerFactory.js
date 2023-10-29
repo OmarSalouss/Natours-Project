137f6321385b43f5a50f200e79b2e3987ff6984e
Omar <omar.salous2@gmail.com>
2023-10-29 10:21:49 +0200

    Building Handler Factory Functions: Reading

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.deleteOne = Model =>
     catchAsync(async (req, res, next) => {
@@ -42,6 +43,49 @@ exports.createOne = Model =>
             status: "Success",
             data: {
                 tour: doc
+            }
+        });
+    });
+
+exports.getOne = (Model, populateOptions) =>
+    catchAsync(async (req, res, next) => {
+        let query = Model.findById(req.params.id);
+        if (populateOptions) query = query.populate(populateOptions);
+        const doc = await query;
+
+        if (!doc) {
+            return next(new AppError('No tour found with that ID', 404));
+        }
+
+        res.status(200).json({
+            status: "Success",
+            // results: tour.length,
+            // requestAT: req.requestTime,
+            data: {
+                data: doc
+            }
+        });
+    });
+
+exports.getAll = Model =>
+    catchAsync(async (req, res, next) => {
+        // To allow for nested GET reviews on tour
+        let filter = {};
+        if (req.params.tourId) filter = { tour: req.params.tourId };
+
+        const features = new APIFeatures(Model.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const doc = await features.query;
+
+        //! SEND RESPONSE
+        res.status(200).json({
+            status: "Success",
+            results: doc.length,
+            data: {
+                data: doc
             }
         });
     });
--- controllers/userController.js
+++ controllers/userController.js
@@ -11,18 +11,6 @@ const filterObj = (obj, ...allowFields) => {
     return newObj;
 }
 
-exports.getAllUsers = catchAsync(async (req, res) => {
-    const users = await User.find();
-
-    //! SEND RESPONSE
-    res.status(200).json({
-        status: "Success",
-        results: users.length,
-        data: {
-            users: users
-        }
-    });
-});
 
 exports.updateMe = catchAsync(async (req, res, next) => {
     // 1) Create error if user POSTs password data
@@ -59,21 +47,12 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 exports.createUser = (req, res) => {
     res.status(500).json({
         status: 'error',
-        message: 'This route is not yet defiend'
+        message: 'This route is yet defiend! Please use /signup instead'
     });
 };
-exports.getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defiend'
-    });
-};
-exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defiend'
-    });
-};
+
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
 
 // Do not update password with this
 exports.updateUser = factory.updateOne(User);
