0aab19d8d3cb9963b7e76068bc73caad4e8cbe90
Omar <omar.salous2@gmail.com>
2023-10-29 11:14:51 +0200

    Importing Review and User Data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -19,15 +21,16 @@ mongose
 
 
 // READ JSON FILE
-const tours = JSON.parse(
-    // fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-    fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 // IMPORT DATA INTO DB
 const importData = async () => {
     try {
         await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
         console.log("Data Successfully Loadaed 😎");
     } catch (error) {
         console.log(error);
@@ -39,6 +42,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log("Data Successfully deleted 😎");
     } catch (error) {
         console.log(error);
