f914665a053edba1ce79beb2515c3695f2a16969
Omar <omar.salous2@gmail.com>
2023-11-02 10:11:10 +0200

    Logging in Users with Our API - Part 2

--- public/img/js/login.js
+++ public/img/js/login.js
@@ -1,24 +1,31 @@
 const login = async (email, password) => {
-    console.log(email, password);
-    try {
-        const res = await axios({
-            method: 'POST',
-            url: 'http://127.0.0.1:3000/api/v1/users/login',
-            data: {
-                email,
-                password
-            }
-        });
-        console.log("11111111111111111");
-    } catch (error) {
-        console.log(error.response.data);
-
+  console.log(email, password);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password
+      }
+    });
+    if (res.data.status === "Success") {
+      alert('Log in Successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
     }
+    console.log("res");
+    console.log(res);
+  } catch (error) {
+    alert(error.response.data.message);
+
+  }
 };
 
 document.querySelector('.form').addEventListener('submit', e => {
-    e.preventDefault();// this prevents the form from la-loading any other page.
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    login(email, password);
+  e.preventDefault();// this prevents the form from la-loading any other page.
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
 });
