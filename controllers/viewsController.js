1d1d491b4135c47225ceb67274ce9ceb7dde6315
Omar <omar.salous2@gmail.com>
2023-11-02 07:23:31 +0200

    Start Building the Template of Login Screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -28,3 +28,14 @@ exports.getTour = catchAsync(async (req, res, next) => {
         tour
     });
 });
+
+exports.getLoginForm = catchAsync(async (req, res, next) => {
+    // 1) Get tour data from collection
+    // const tours = await Tour.find();
+
+    // 2) Build Template
+    // 3) Render that template using tour data from 1)
+    res.status(200).render('login', {
+        title: 'Log into your account',
+    });
+});
