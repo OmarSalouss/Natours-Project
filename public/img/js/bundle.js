6e6a5580082f57d713083dae6a7209e4675a2165
Omar <omar.salous2@gmail.com>
2023-11-02 16:30:56 +0200

    Rendering Error Pages

--- public/img/js/bundle.js
+++ public/img/js/bundle.js
@@ -12275,7 +12275,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11256" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13504" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
